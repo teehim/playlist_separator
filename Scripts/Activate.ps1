5bcf6a5d2fd82af4d33c404e081f939e7864edd7
teehim <teehim.thanatkorn@gmail.com>
2020-10-26 15:45:23 +0700

    update

--- Scripts/Activate.ps1
+++ Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="E:\Code\playlist_seperator"
+$env:VIRTUAL_ENV="C:\Users\Exzy\Documents\GitHub\playlist_separator"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
@@ -35,7 +35,7 @@ if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     function global:_OLD_VIRTUAL_PROMPT {""}
     copy-item function:prompt function:_OLD_VIRTUAL_PROMPT
     function global:prompt {
-        Write-Host -NoNewline -ForegroundColor Green '(playlist_seperator) '
+        Write-Host -NoNewline -ForegroundColor Green '(playlist_separator) '
         _OLD_VIRTUAL_PROMPT
     }
 }
